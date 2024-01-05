85efa59a543ac3037a6f67d72a300607d3da1a6b
Kody <kodyckendall@gmail.com>
2024-01-05 09:46:56 -0700

    Move random_agent into a new folder structure, and implement run_game where white and black can have their own different agents

---
+++ main/run_game.py
@@ -0,0 +1,27 @@
+import chess
+from random_agent import RandomAgent
+# from smart_agent import SmartAgent
+
+if __name__ == "__main__":
+    board = chess.Board()
+    white_agent = RandomAgent(chess.WHITE)
+    black_agent = RandomAgent(chess.BLACK) 
+    
+    # white_agent = SmartAgent(chess.WHITE)
+    # black_agent = SmartAgent(chess.BLACK)
+
+    while (not board.is_checkmate() and not board.is_stalemate() and not board.is_insufficient_material() and not board.is_fivefold_repetition()):
+        
+        if (board.turn == chess.WHITE):
+            next_move = white_agent.find_next_move(board)
+            board.push(next_move)
+        else:
+            next_move = black_agent.find_next_move(board)
+            board.push(next_move)
+        
+        print(board)
+        # input() #<-- uncomment to watch this game in real time (press enter in terminal to go to the next move)
+    
+    #winner is!
+    print (board.outcome().termination)
+    print("White!" if board.outcome().winner else ("Black!" if board.outcome().winner == False else "Draw!"))
